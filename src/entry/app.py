0c33e5891a626d89478169dc8bb9fd5d4540cb89
Steve Wyckoff <stevewyckoffdev@gmail.com>
2024-10-15 21:38:17 -0700

    Add retry to entry

--- src/entry/app.py
+++ src/entry/app.py
@@ -1,4 +1,7 @@
+from logging import WARNING
+
 from attr import dataclass
+from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
 
 from common.better_logger import (
     log_critical,
@@ -6,6 +9,7 @@ from common.better_logger import (
     log_error,
     log_info,
     log_warning,
+    logger,
 )
 from common.settings import settings
 
@@ -16,6 +20,15 @@ class MyClass:
     host: str
     port: int
 
+    @retry(
+        stop=stop_after_attempt(3),
+        wait=wait_fixed(2),
+        before_sleep=before_sleep_log(logger, WARNING),  # Log before each retry
+    )
+    def run(self) -> None:
+        pass
+
 
 def run():
     my_class = MyClass(settings.host, settings.port)
+    my_class.run()
