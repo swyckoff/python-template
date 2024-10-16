9cc931e058bf700c22f004b2d38b07bfa1cd6b6b
Steve Wyckoff <stevewyckoffdev@gmail.com>
2024-10-16 03:10:43 -0700

    Squelch tenacity type issue

--- src/entry/app.py
+++ src/entry/app.py
@@ -1,16 +1,15 @@
 from logging import WARNING
 
 from attr import dataclass
-from tenacity import before_sleep_log, retry, stop_after_attempt, wait_fixed
+from tenacity import retry  # type: ignore
+from tenacity import before_sleep_log, stop_after_attempt, wait_fixed
 
-from common.better_logger import (
-    log_critical,
-    log_debug,
-    log_error,
-    log_info,
-    log_warning,
-    logger,
-)
+from common.better_logger import log_critical  # noqa: F401
+from common.better_logger import log_debug  # noqa: F401
+from common.better_logger import log_error  # noqa: F401
+from common.better_logger import log_info  # noqa: F401
+from common.better_logger import log_warning  # noqa: F401
+from common.better_logger import logger
 from common.settings import settings
 
 
@@ -23,7 +22,7 @@ class MyClass:
     @retry(
         stop=stop_after_attempt(3),
         wait=wait_fixed(2),
-        before_sleep=before_sleep_log(logger, WARNING),  # Log before each retry
+        before_sleep=before_sleep_log(logger, WARNING),
     )
     def run(self) -> None:
         pass
